b475604adc390dda0bbe8e163508935c17bfa24e
Sieger06 <irek.haibulin@gmail.com>
2023-05-27 12:25:01 +0300

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) throws SQLException {
+        final String username = "postgres";
+        final String password = "[Sun0mu66]";
+    }
+}
