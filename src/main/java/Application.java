ab507bbdfee00fdfaf526cb75941d36897375e19
Sieger06 <irek.haibulin@gmail.com>
2023-07-10 13:37:56 +0300

    done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,39 +1,51 @@
+import DAO.CityDAO;
+import DAO.CityDAOImpl;
+import DAO.EmployeeDAO;
 import DAO.EmployeeDAOImpl;
+import model.City;
 import model.Employee;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
-        final String username ="postgres";
-        final String password = "[SunOmu66]";
-        final String url = "jdbc:postgresql://localhost:5432/skypro";
 
-        Connection connection = DriverManager.getConnection(url, username, password);
-        PreparedStatement statement = connection.prepareStatement(
-                "SELECT employee.first_name, employee.last_name, employee.age, city.city_name" +
-                        " FROM employee " +
-                        "inner join city on employee.city_id = city.city_id" +
-                        " WHERE employee.id = (?)" );
+        EmployeeDAO employees = new EmployeeDAOImpl();
+        CityDAO cities  = new CityDAOImpl();
 
-        statement.setInt(1, 6);
 
-        final ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()){
-            System.out.println("first_name: " + resultSet.getString("first_name") +
-                    "; last_name: " + resultSet.getString("last_name") +
-                    "; age: " + resultSet.getInt("age") +
-                    "; city: " + resultSet.getString("city_name") + ";");
-        }
+        Employee e1 = new Employee();
+        e1.setFirstname("Peter");
+        e1.setLastName("Petrov");
+        e1.setAge(8);
+        e1.setGender("m");
 
-        EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl(connection);
+        Employee e2 = new Employee();
+        e2.setFirstname("Ivan");
+        e2.setLastName("Ivanov");
+        e2.setAge(54);
+        e2.setGender("m");
 
-        System.out.println(employeeDAO.getById(6));
-        System.out.println(employeeDAO.getAll());
-        employeeDAO.removeById(14);
-        employeeDAO.create(employeeDAO.getById(3));
-        employeeDAO.updateById(15, employeeDAO.getById(2));
-        System.out.println(employeeDAO.getById(7));
+        City city = new City();
+        city.setCityName("Paris");
+        city.setEmployees(new ArrayList<Employee>(Arrays.asList(e1, e2)));
+
+        cities.create(city);
+        System.out.println(city);
+
+
+        City c = cities.getById(8);
+
+        e1.setCity(c);
+        e2.setCity(c);
+        employees.update(e1);
+        employees.update(e2);
+
+
+        cities.removeById(8);
 
 
     }
